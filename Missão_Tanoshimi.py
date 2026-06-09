ef9c3a7cf9f2d621563d08d7bb29495fcc1d9613
Pedro Ivo Christianes de Oliveira e Silva <pedro.brasil.2028@gmail.com>
2026-06-08 21:39:06 -0300

    primeiro Commit

---
+++ Missão_Tanoshimi.py
@@ -0,0 +1,19 @@
+def mostrar_cardapio():
+  cardapio = {"Makimono":50.00, "sushi": 50.00, "Tempurá de frutos do mar": 25.00, "Tempurá de Sorvete": 28.00, "Saquê": 40.00 }
+  
+  print(f"-----cardapio----- \n")
+  for item, precos in cardapio.items():
+    print(F"Temos o produto: {item} valor: {precos}")
+  return cardapio 
+
+
+mostrar_cardapio()
+
+
+#def fazer_pedido():
+#   pedido = []
+#    total = 0.0
+
+#    item_escolhido = 
+
+
